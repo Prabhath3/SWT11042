5ce53e51fbdf33380ae38d9a66322f606aecea92
Prabhath3 <prabhathnishantha130@gmail.com>
2025-02-19 09:34:01 +0530

    upatde first.c

---
+++ demo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+  printf("Welcome to fundamentals of programming");
+
+  return 0;
+}
--- first.c
+++ first.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main() {
-  printf("Welcome to fundamentals of programming");
+  printf("welcome to fundamentals of programming session\n");
+  printf("Prabhath nishantha, SEU/IS/22/ICT/041\n");
+  printf("Gnanachandran sanojan, SEU/IS/21/ICT/064");
 
   return 0;
 }
