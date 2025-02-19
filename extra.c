ed86d6bdcb18f053166a55e5992e83fc86acdc44
Prabhath3 <prabhathnishantha130@gmail.com>
2025-02-19 09:56:06 +0530

    file are updated and created

--- extra.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main () {
-  printf("hbvjfdsaiggfd,bh");
-
-  return 0;
-}
---
+++ first1.c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  printf("welcome to fundamentals of programming session\n");
+  printf("Prabhath nishantha, SEU/IS/22/ICT/041\n");
+  printf("Gnanachandran sanojan, SEU/IS/21/ICT/064");
+  printf("2025-02-20");
+
+  return 0;
+}
