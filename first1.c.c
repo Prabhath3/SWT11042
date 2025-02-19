8492d7003dccd7892badef23c2fa84da5d97931f
Prabhath3 <prabhathnishantha130@gmail.com>
2025-02-19 10:19:37 +0530

    updated

--- first1.c.c
+++ first1.c.c
@@ -3,7 +3,7 @@ int main() {
   printf("welcome to fundamentals of programming session\n");
   printf("Prabhath nishantha, SEU/IS/22/ICT/041\n");
   printf("Gnanachandran sanojan, SEU/IS/21/ICT/064");
-  printf("2025-02-20");
+  printf("2025-02-2");
 
   return 0;
 }
